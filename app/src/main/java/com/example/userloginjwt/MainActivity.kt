ddda5899d8e22fc7cc632791ee5e227ca2b3cb8b
klosik007 <pklos1992@gmail.com>
2021-05-29 01:16:02 +0200

    view Biding support in Mainactivity

--- app/src/main/java/com/example/userloginjwt/MainActivity.kt
+++ app/src/main/java/com/example/userloginjwt/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.userloginjwt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.userloginjwt.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.json.JSONObject
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var JSONObj: JSONObject
+    private val URL: String = "localhost/JWT_Auth/index.php"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.loginBtn.setOnClickListener{
+            //login to service
+        }
+
+        binding.signupBtn.setOnClickListener{
+            //sign up new user
+        }
     }
+
+
 }
