4514721a98a892bd77680a9c8227e84ebf7af499
klosik007 <pklos1992@gmail.com>
2021-05-29 20:56:56 +0200

    JSONParser initial commit: sketch

---
+++ app/src/main/java/com/example/userloginjwt/JSONParser.kt
@@ -0,0 +1,13 @@
+package com.example.userloginjwt
+
+import org.json.JSONObject
+
+class JSONParser {
+    companion object{
+        fun makeHttpRequest(url: String, type: String, data: Map<String, String>): JSONObject? {
+
+            return null
+        }
+    }
+
+}
